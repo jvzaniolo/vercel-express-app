8a7878314eb271dc0cf00553d348417a8f56cbbc
João Vitor <jv.zaniolo@gmail.com>
2024-05-22 13:31:01 -0300

    add /products

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/products", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app;
