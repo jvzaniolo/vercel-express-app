1c88e8bd3466301a8302c0564e429504500c08c1
João Vitor <jv.zaniolo@gmail.com>
2024-05-22 13:29:12 -0300

    test with ESM

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -8,4 +8,4 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+export default app;
